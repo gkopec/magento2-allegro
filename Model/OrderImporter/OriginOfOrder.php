07c556b53bc7a0a434aa778087d3d138ee766855
Genius-Dev <contact@genius-dev.pl>
2022-10-25 21:39:05 +0200

    PHP8.1 fixes

--- Model/OrderImporter/OriginOfOrder.php
+++ Model/OrderImporter/OriginOfOrder.php
@@ -18,12 +18,12 @@ class OriginOfOrder
      */
     public function isOrderFromAllegro(Order $order)
     {
-        if (stripos($order->getOrderFrom(), self::ALLEGRO) !== false) {
+        if (!$order->getOrderFrom() || stripos($order->getOrderFrom(), self::ALLEGRO) !== false) {
             return true;
         }
 
         $extensionAttributes = $order->getExtensionAttributes();
-        if ($extensionAttributes && stripos($extensionAttributes->getOrderFrom(), self::ALLEGRO) !== false) {
+        if ($extensionAttributes && stripos($extensionAttributes->getOrderFrom() ?? '', self::ALLEGRO) !== false) {
             return true;
         }
 
